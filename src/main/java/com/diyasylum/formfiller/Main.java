7455a9e446031e0ea521d7329689a1fd01c7cda3
Matt Bachmann <bachmann.matt@gmail.com>
2018-12-12 22:50:16 -0500

    DIYAS-1 initial commit

---
+++ src/main/java/com/diyasylum/formfiller/Main.java
@@ -0,0 +1,10 @@
+package com.diyasylum.formfiller;
+
+public class Main {
+    public String hello() {
+        return "hello";
+    }
+    public static void main(String[] args) {
+        System.out.println(new Main().hello());
+    }
+}
---
+++ src/test/java/com/diyasylum/formfiller/MainTest.java
@@ -0,0 +1,13 @@
+package com.diyasylum.formfiller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void hello() {
+        assertEquals("hello", new Main().hello());
+    }
+}
