559c898951f10feeebc5be25224a60a941013cee
Matt Bachmann <bachmann.matt@gmail.com>
2018-12-12 23:00:51 -0500

    DIYAS-1 push a commit to see if I setup travis right. expect a few of these

---
+++ src/main/java/com/diyasylum/formfiller/i589Filler.java
@@ -0,0 +1,7 @@
+package com.diyasylum.formfiller;
+
+public class i589Filler {
+    public static final String SUPPORTED_HASH = "";
+    public static final String SUPPORTED_FORM_EDITION_DATE = "05/16/17";
+
+}
---
+++ src/test/java/com/diyasylum/formfiller/i589FillerTest.java
@@ -0,0 +1,15 @@
+package com.diyasylum.formfiller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class i589FillerTest {
+
+    @Test
+    void assertVersionIsCurrent() {
+        // This code is functionally useless if it is out of sync with the current form
+        
+    }
+
+}
