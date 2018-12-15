db4ebe31bb59a734e3ab3786d71e0164ad06d82d
Matt Bachmann <bachmann.matt@gmail.com>
2018-12-15 17:20:27 -0500

    DIYAS-1 add logic to fill in the form

--- src/main/java/com/diyasylum/formfiller/I589Filler.java
+++ src/main/java/com/diyasylum/formfiller/I589Filler.java
@@ -1,11 +1,11 @@
 package com.diyasylum.formfiller;
 
 import com.diyasylum.formfiller.models.I589Field;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
-import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDTerminalField;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -21,8 +21,13 @@ class I589Filler {
     return new I589Filler(pdDocument);
   }
 
+  /**
+   * Creates a filler based on a provided byte array representing a pdf calling this removes any
+   * existing security on the document
+   */
   static I589Filler fromi589PdfBytes(byte[] i589pdf) throws IOException {
     PDDocument pdDoc = PDDocument.load(i589pdf);
+    pdDoc.setAllSecurityToBeRemoved(true);
     return new I589Filler(pdDoc);
   }
 
@@ -30,11 +35,47 @@ class I589Filler {
     this.i589Pdf = i589pdf;
   }
 
+  /*
+   I like the idea of the passed in document being immutable.
+   So for filling in I would prefer to use a new document.
+   If this proves stupid slow I can scrap the idea
+  */
+  private PDDocument cloneI589() throws IOException {
+    ByteArrayOutputStream copyStream = new ByteArrayOutputStream();
+    i589Pdf.save(copyStream);
+    return PDDocument.load(copyStream.toByteArray());
+  }
+
+  /**
+   * Method makes a best faith effort to fill in the form based on the fields provided. This
+   * operation clears out the original security of the form
+   *
+   * @param fields list of I589 fields. Unrecognized fields will be ignored. Method will fill in any
+   *     field it can.
+   * @return byte[] with the contents of the form.
+   * @throws IOException if there are problems working with the pdf
+   */
+  byte[] fillInForm(List<I589Field> fields) throws IOException {
+    Map<String, String> fieldToValue =
+        fields.stream().collect(Collectors.toMap(I589Field::getAbsolutePath, I589Field::getValue));
+
+    PDDocument filledInForm = cloneI589();
+
+    for (PDField pdField : filledInForm.getDocumentCatalog().getAcroForm().getFieldTree()) {
+      if (fieldToValue.containsKey(pdField.getFullyQualifiedName())) {
+        pdField.setValue(fieldToValue.get(pdField.getFullyQualifiedName()));
+      }
+    }
+
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    filledInForm.save(byteArrayOutputStream);
+    filledInForm.close();
+    return byteArrayOutputStream.toByteArray();
+  }
+
   List<I589Field> extractFields() {
     List<I589Field> fields = new ArrayList<>();
-    PDDocumentCatalog pdCatalog = i589Pdf.getDocumentCatalog();
-    PDAcroForm pdAcroForm = pdCatalog.getAcroForm();
-    for (PDField pdField : pdAcroForm.getFieldTree()) {
+    for (PDField pdField : i589Pdf.getDocumentCatalog().getAcroForm().getFieldTree()) {
       if (pdField instanceof PDTerminalField) {
         fields.add(I589Field.fromPdField(pdField));
       }
