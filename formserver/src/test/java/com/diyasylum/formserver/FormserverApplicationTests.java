8e7e84bacd72c34cad401079a289b21e42fe8767
Matt Bachmann <bachmann.matt@gmail.com>
2018-12-18 21:07:54 -0500

    DIYAS-2 add spring boot server

---
+++ formserver/src/test/java/com/diyasylum/formserver/FormserverApplicationTests.java
@@ -0,0 +1,19 @@
+package com.diyasylum.formserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
