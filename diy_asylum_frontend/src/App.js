88a074dc130fdc79b394a76d32ec2c7d61e76960
JeuelyFish <jbernardwilkerson@gmail.com>
2018-12-20 08:37:03 -0800

    i18n is working

--- diy_asylum_frontend/src/App.js
+++ diy_asylum_frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import MainView from "./components/MainView";
@@ -11,11 +11,16 @@ import translations from "./i18n/locales";
 import "./App.scss";
 
 class App extends Component {
+  // when the app mounts, before rendering add the locale i18n data
+  componentDidMount() {
+    addLocaleData(enLocaleData);
+    addLocaleData(esLocaleData);
+  }
+
+  // then render and pass the correct locale messages to the IntlProvider
   render() {
     const { locale } = this.props;
-    console.log(locale)
     const messages = translations[locale];
-    console.log(messages)
     return (
       <IntlProvider locale={locale} key={locale} messages={messages}>
         <div className="App">
@@ -30,15 +35,6 @@ class App extends Component {
     );
   }
 }
-// export default () => (
-//   <IntlProvider locale={locale} key={locale} messages={messages}>
-//     <div className="App">
-//       <Router>
-//         <Route path="/:filter?" render={({ match }) => (<MainView filter={match.params.filter} />)} />
-//       </Router>
-//     </div>
-//   </IntlProvider>
-// );
 
 const mapStateToProps = state => ({
   locale: state.user.locale
