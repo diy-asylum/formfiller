b747986a69eb84e43a081bfd4095beb935e4e7e3
Mike <michael.c.kreisel@gmail.com>
2018-12-29 18:06:41 -0500

    added comments and refactored

--- ops/fe_deploy.tf
+++ ops/fe_deploy.tf
@@ -1,9 +1,13 @@
+# AWS credentials are automatically loaded from the default AWS CLI configuration
 provider "aws" {}
 
+# Fetches the data from ECR for the "frontend" Docker registry
+# Assumes that such a registry already exists
 data "aws_ecr_repository" "diy-ecr-fe" {
   name = "frontend"
 }
 
+# Creates IAM policies with permissions suitable for running ECS tasks
 resource "aws_iam_role" "ecs_execution_role" {
   name = "ecs_execution_role"
 
@@ -49,43 +53,9 @@ resource "aws_iam_role_policy" "ecs_execution_policy" {
 EOF
 }
 
-resource "aws_ecs_cluster" "diyasylum" {
-  name = "diyasylum"
-}
-
-resource "aws_ecs_task_definition" "diyasylum-fe" {
-  family                   = "diyasylum-fe"
-  depends_on               = ["aws_iam_role_policy.ecs_execution_policy"]
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = 1024
-  memory                   = 4096
-  execution_role_arn       = "${aws_iam_role.ecs_execution_role.arn}"
-
-  container_definitions = <<DEFINITION
-[
-  {
-    "essential": true,
-    "image": "${data.aws_ecr_repository.diy-ecr-fe.repository_url}:latest",
-    "name": "diyasylum-fe",
-    "portMappings": [
-                {
-                    "containerPort": 3000,
-                    "hostPort": 3000
-                }
-            ]
-  }
-]
-DEFINITION
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.main.id}"
-}
+# Overly permissive security group allowing public access 
+# to all ports we care about. We will eventually need a more
+# comprehensive security setup.
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
@@ -114,6 +84,12 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
+# Create private VPC to host our service.
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Declare subnets in which our service will be hosted.
 resource "aws_subnet" "subnet_1" {
   vpc_id            = "${aws_security_group.allow_all.vpc_id}"
   cidr_block        = "10.0.0.0/25"
@@ -126,16 +102,18 @@ resource "aws_subnet" "subnet_2" {
   availability_zone = "us-east-1b"
 }
 
-resource "aws_route_table" "nat_route_1" {
+# Create an internet gateway to allow our cluster to access
+# the public internet. See why we need this just below.
+resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
-  }
 }
 
-resource "aws_route_table" "nat_route_2" {
+# Create a route from the subnet to our internet gateway so that
+# services within the subnet can access the public internet.
+# This is required so that the ECS service can pull Docker containers
+# from ECR. They're both AWS services, so why can't ECR be hosted within the subnet
+# to avoid public internet access? But apparently it has to be this way.
+resource "aws_route_table" "nat_route" {
   vpc_id = "${aws_vpc.main.id}"
 
   route {
@@ -146,14 +124,15 @@ resource "aws_route_table" "nat_route_2" {
 
 resource "aws_route_table_association" "subnet_1_association" {
   subnet_id      = "${aws_subnet.subnet_1.id}"
-  route_table_id = "${aws_route_table.nat_route_1.id}"
+  route_table_id = "${aws_route_table.nat_route.id}"
 }
 
 resource "aws_route_table_association" "subnet_2_association" {
   subnet_id      = "${aws_subnet.subnet_2.id}"
-  route_table_id = "${aws_route_table.nat_route_2.id}"
+  route_table_id = "${aws_route_table.nat_route.id}"
 }
 
+# Create a target group for load balancing.
 resource "aws_lb_target_group" "ecs-target-group" {
   name        = "diyasylum-fe"
   port        = 3000
@@ -162,6 +141,7 @@ resource "aws_lb_target_group" "ecs-target-group" {
   vpc_id      = "${aws_vpc.main.id}"
 }
 
+# Create a load balancer.
 resource "aws_lb" "ecs-load-balancer" {
   name            = "ecs-load-balancer"
   security_groups = ["${aws_security_group.allow_all.id}"]
@@ -172,6 +152,12 @@ resource "aws_lb" "ecs-load-balancer" {
   }
 }
 
+# Prints load balancer DNS when deployment is complete.
+output "lb_dns" {
+  value = "${aws_lb.ecs-load-balancer.dns_name}"
+}
+
+# Direct load balancer to forward to target group.
 resource "aws_lb_listener" "lb-listener" {
   load_balancer_arn = "${aws_lb.ecs-load-balancer.arn}"
   port              = "80"
@@ -183,6 +169,42 @@ resource "aws_lb_listener" "lb-listener" {
   }
 }
 
+# Define parameters of ECS task. Indicates the Docker container which will be run,
+# port mappings, required compute resources, and permissions.
+resource "aws_ecs_task_definition" "diyasylum-fe" {
+  family                   = "diyasylum-fe"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 1024
+  memory                   = 4096
+  execution_role_arn       = "${aws_iam_role.ecs_execution_role.arn}"
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "essential": true,
+    "image": "${data.aws_ecr_repository.diy-ecr-fe.repository_url}:latest",
+    "name": "diyasylum-fe",
+    "portMappings": [
+                {
+                    "containerPort": 3000,
+                    "hostPort": 3000
+                }
+            ]
+  }
+]
+DEFINITION
+
+  depends_on = ["aws_iam_role_policy.ecs_execution_policy"]
+}
+
+# Create ECS cluster.
+resource "aws_ecs_cluster" "diyasylum" {
+  name = "diyasylum"
+}
+
+# Create ECS service to run the task defined above. Service will be accessible
+# via the load balancer defined above. 
 resource "aws_ecs_service" "diyasylum-fe" {
   name          = "diyasylum-fe"
   cluster       = "${aws_ecs_cluster.diyasylum.id}"
@@ -195,7 +217,6 @@ resource "aws_ecs_service" "diyasylum-fe" {
     subnets          = ["${aws_subnet.subnet_1.id}", "${aws_subnet.subnet_2.id}"]
   }
 
-  # Track the latest ACTIVE revision
   task_definition = "${aws_ecs_task_definition.diyasylum-fe.family}"
 
   load_balancer {
