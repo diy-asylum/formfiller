efbff030749d8af9e75439e31b7217f8f14f53bb
RIchard Rast <richard.rast@gmail.com>
2019-01-15 08:45:22 -0500

    Fix double render, address a couple lint issues

--- diy_asylum_frontend/src/components/FormContentComponent/index.js
+++ diy_asylum_frontend/src/components/FormContentComponent/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 const inputMaker = ({ inputType, elementName, help }, helpTextSetter) => {
   const onfocus = () => helpTextSetter(help);
