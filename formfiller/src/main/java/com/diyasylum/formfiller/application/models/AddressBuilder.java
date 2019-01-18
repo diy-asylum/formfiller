681e2d16312a2eab2f3010cb956ba68d972e7688
Matt Bachmann <bachmann.matt@gmail.com>
2019-01-17 23:01:12 -0500

    Move common methods to the abstract mapper

--- formfiller/src/main/java/com/diyasylum/formfiller/application/models/AddressBuilder.java
+++ formfiller/src/main/java/com/diyasylum/formfiller/application/models/AddressBuilder.java
@@ -1,62 +1,71 @@
 package com.diyasylum.formfiller.application.models;
 
 public class AddressBuilder {
-    private String inCareOf;
-    private String streetName;
-    private String streetNumber;
-    private String apartmentNumber;
-    private String city;
-    private String state;
-    private String zipCode;
-    private String areaCode;
-    private String phoneNumber;
+  private String inCareOf;
+  private String streetName;
+  private String streetNumber;
+  private String apartmentNumber;
+  private String city;
+  private String state;
+  private String zipCode;
+  private String areaCode;
+  private String phoneNumber;
 
-    public AddressBuilder setInCareOf(String inCareOf) {
-        this.inCareOf = inCareOf;
-        return this;
-    }
+  public AddressBuilder setInCareOf(String inCareOf) {
+    this.inCareOf = inCareOf;
+    return this;
+  }
 
-    public AddressBuilder setStreetName(String streetName) {
-        this.streetName = streetName;
-        return this;
-    }
+  public AddressBuilder setStreetName(String streetName) {
+    this.streetName = streetName;
+    return this;
+  }
 
-    public AddressBuilder setStreetNumber(String streetNumber) {
-        this.streetNumber = streetNumber;
-        return this;
-    }
+  public AddressBuilder setStreetNumber(String streetNumber) {
+    this.streetNumber = streetNumber;
+    return this;
+  }
 
-    public AddressBuilder setApartmentNumber(String apartmentNumber) {
-        this.apartmentNumber = apartmentNumber;
-        return this;
-    }
+  public AddressBuilder setApartmentNumber(String apartmentNumber) {
+    this.apartmentNumber = apartmentNumber;
+    return this;
+  }
 
-    public AddressBuilder setCity(String city) {
-        this.city = city;
-        return this;
-    }
+  public AddressBuilder setCity(String city) {
+    this.city = city;
+    return this;
+  }
 
-    public AddressBuilder setState(String state) {
-        this.state = state;
-        return this;
-    }
+  public AddressBuilder setState(String state) {
+    this.state = state;
+    return this;
+  }
 
-    public AddressBuilder setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-        return this;
-    }
+  public AddressBuilder setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+    return this;
+  }
 
-    public AddressBuilder setAreaCode(String areaCode) {
-        this.areaCode = areaCode;
-        return this;
-    }
+  public AddressBuilder setAreaCode(String areaCode) {
+    this.areaCode = areaCode;
+    return this;
+  }
 
-    public AddressBuilder setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-        return this;
-    }
+  public AddressBuilder setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+    return this;
+  }
 
-    public Address createAddress() {
-        return new Address(inCareOf, streetName, streetNumber, apartmentNumber, city, state, zipCode, areaCode, phoneNumber);
-    }
+  public Address createAddress() {
+    return new Address(
+        inCareOf,
+        streetName,
+        streetNumber,
+        apartmentNumber,
+        city,
+        state,
+        zipCode,
+        areaCode,
+        phoneNumber);
+  }
 }
--- formfiller/src/main/java/com/diyasylum/formfiller/i589/I589ApplicationMapping.java
+++ formfiller/src/main/java/com/diyasylum/formfiller/i589/I589ApplicationMapping.java
@@ -1,9 +1,9 @@
 package com.diyasylum.formfiller.i589;
 
 import com.diyasylum.formfiller.application.models.*;
+import com.diyasylum.formfiller.mappings.AbstractMapper;
 import java.util.*;
 import java.util.function.Function;
-import org.apache.commons.lang3.StringUtils;
 
 abstract class I589ApplicationMapping {
   // Right now this is mapping to string. This may eventually map to an object taking in a string
@@ -15,15 +15,15 @@ abstract class I589ApplicationMapping {
     Map<String, Function<I589Application, String>> pdfFieldMap = new HashMap<>();
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine1_ANumber[0]",
-        a -> noneIfBlank(a.getApplicantInfo().getAlienRegistrationNumber()));
+        a -> AbstractMapper.noneIfBlank(a.getApplicantInfo().getAlienRegistrationNumber()));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField1[0]", a -> a.getApplicantInfo().getSocialSecurityNumber());
     pdfFieldMap.put(
         "form1[0].#subform[10].USCISOnlineAcctNumber[0]",
-        a -> noneIfBlank(a.getApplicantInfo().getUSISAccountNumber()));
+        a -> AbstractMapper.noneIfBlank(a.getApplicantInfo().getUSISAccountNumber()));
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine4_LastName[0]",
-        a -> naIfBlank(a.getApplicantInfo().getLastName()));
+        a -> AbstractMapper.naIfBlank(a.getApplicantInfo().getLastName()));
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine8_StreetNumandName[0]",
         a ->
@@ -42,10 +42,10 @@ abstract class I589ApplicationMapping {
         "form1[0].#subform[0].TextField1[3]", a -> a.getApplicantInfo().getPresentNationality());
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine5_FirstName[0]",
-        a -> naIfBlank(a.getApplicantInfo().getFirstName()));
+        a -> AbstractMapper.naIfBlank(a.getApplicantInfo().getFirstName()));
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine6_MiddleName[0]",
-        a -> naIfBlank(a.getApplicantInfo().getMiddleName()));
+        a -> AbstractMapper.naIfBlank(a.getApplicantInfo().getMiddleName()));
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine8_AreaCode[0]",
         a -> a.getApplicantInfo().getUsResidence().getAreaCode());
@@ -82,23 +82,30 @@ abstract class I589ApplicationMapping {
         a -> a.getApplicantInfo().getUsMailingAddress().getState());
     pdfFieldMap.put(
         "form1[0].#subform[0].PartALine9Gender[0]",
-        a -> formatCheckbox(a.getApplicantInfo().getGender() == Gender.MALE, "M"));
+        a -> AbstractMapper.formatCheckbox(a.getApplicantInfo().getGender() == Gender.MALE, "M"));
     pdfFieldMap.put(
         "form1[0].#subform[0].PartALine9Gender[1]",
-        a -> formatCheckbox(a.getApplicantInfo().getGender() == Gender.FEMALE, "F"));
+        a -> AbstractMapper.formatCheckbox(a.getApplicantInfo().getGender() == Gender.FEMALE, "F"));
     pdfFieldMap.put(
         "form1[0].#subform[0].Marital[0]",
-        a -> formatCheckbox(a.getApplicantInfo().getMaritalStatus() == MaritalStatus.SINGLE, "S"));
+        a ->
+            AbstractMapper.formatCheckbox(
+                a.getApplicantInfo().getMaritalStatus() == MaritalStatus.SINGLE, "S"));
     pdfFieldMap.put(
         "form1[0].#subform[0].Marital[1]",
-        a -> formatCheckbox(a.getApplicantInfo().getMaritalStatus() == MaritalStatus.MARRIED, "M"));
+        a ->
+            AbstractMapper.formatCheckbox(
+                a.getApplicantInfo().getMaritalStatus() == MaritalStatus.MARRIED, "M"));
     pdfFieldMap.put(
         "form1[0].#subform[0].Marital[2]",
         a ->
-            formatCheckbox(a.getApplicantInfo().getMaritalStatus() == MaritalStatus.DIVORCED, "D"));
+            AbstractMapper.formatCheckbox(
+                a.getApplicantInfo().getMaritalStatus() == MaritalStatus.DIVORCED, "D"));
     pdfFieldMap.put(
         "form1[0].#subform[0].Marital[3]",
-        a -> formatCheckbox(a.getApplicantInfo().getMaritalStatus() == MaritalStatus.WIDOWED, "W"));
+        a ->
+            AbstractMapper.formatCheckbox(
+                a.getApplicantInfo().getMaritalStatus() == MaritalStatus.WIDOWED, "W"));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField1[4]",
         a ->
@@ -115,20 +122,20 @@ abstract class I589ApplicationMapping {
     pdfFieldMap.put(
         "form1[0].#subform[0].CheckBox3[0]",
         a ->
-            formatCheckbox(
+            AbstractMapper.formatCheckbox(
                 a.getApplicantInfo().getImmigrationCourtHistory() == ImmigrationCourtHistory.NEVER,
                 "A"));
     pdfFieldMap.put(
         "form1[0].#subform[0].CheckBox3[1]",
         a ->
-            formatCheckbox(
+            AbstractMapper.formatCheckbox(
                 a.getApplicantInfo().getImmigrationCourtHistory()
                     == ImmigrationCourtHistory.NOT_NOW_BUT_IN_THE_PAST,
                 "C"));
     pdfFieldMap.put(
         "form1[0].#subform[0].CheckBox3[2]",
         a ->
-            formatCheckbox(
+            AbstractMapper.formatCheckbox(
                 a.getApplicantInfo().getImmigrationCourtHistory()
                     == ImmigrationCourtHistory.CURRENTLY,
                 "B"));
@@ -137,73 +144,75 @@ abstract class I589ApplicationMapping {
         a -> a.getUsTravelHistory().getLastLeftHomeCountry());
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField3[0]",
-        a -> noneIfBlank(a.getUsTravelHistory().getI94Number()));
+        a -> AbstractMapper.noneIfBlank(a.getUsTravelHistory().getI94Number()));
     pdfFieldMap.put(
         "form1[0].#subform[0].DateTimeField2[0]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 0)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 0)
                 .map(UsTravelEvent::getDate)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField4[0]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 0)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 0)
                 .map(UsTravelEvent::getPlace)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField4[1]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 0)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 0)
                 .map(UsTravelEvent::getStatus)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].DateTimeField3[0]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 1)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 1)
                 .map(UsTravelEvent::getDate)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField4[2]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 1)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 1)
                 .map(UsTravelEvent::getPlace)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField4[3]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 1)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 1)
                 .map(UsTravelEvent::getStatus)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].DateTimeField4[0]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 2)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 2)
                 .map(UsTravelEvent::getDate)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField4[4]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 2)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 2)
                 .map(UsTravelEvent::getPlace)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField4[5]",
         a ->
-            safeListAccess(a.getUsTravelHistory().getTravelEvents(), 2)
+            AbstractMapper.safeListAccess(a.getUsTravelHistory().getTravelEvents(), 2)
                 .map(UsTravelEvent::getStatus)
                 .orElse(""));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField5[0]",
-        a -> noneIfBlank(a.getApplicantInfo().getCountryWhoLastIssuedPassport()));
+        a -> AbstractMapper.noneIfBlank(a.getApplicantInfo().getCountryWhoLastIssuedPassport()));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField5[1]",
-        a -> noneIfBlank(a.getApplicantInfo().getPassportNumber()));
+        a -> AbstractMapper.noneIfBlank(a.getApplicantInfo().getPassportNumber()));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField5[2]",
-        a -> noneIfBlank(a.getApplicantInfo().getTravelDocumentNumber()));
+        a -> AbstractMapper.noneIfBlank(a.getApplicantInfo().getTravelDocumentNumber()));
     pdfFieldMap.put(
         "form1[0].#subform[0].CheckBox31[0]",
-        a -> formatCheckbox(a.getApplicantInfo().isAlsoApplyingConventionAgainstTorture(), "1"));
+        a ->
+            AbstractMapper.formatCheckbox(
+                a.getApplicantInfo().isAlsoApplyingConventionAgainstTorture(), "1"));
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine9_AreaCode[0]",
         a -> a.getApplicantInfo().getUsMailingAddress().getAreaCode());
@@ -213,55 +222,27 @@ abstract class I589ApplicationMapping {
     pdfFieldMap.put("form1[0].#subform[0].DateTimeField2[1]", a -> "");
     pdfFieldMap.put(
         "form1[0].#subform[0].DateTimeField2[2]",
-        a -> naIfBlank(a.getApplicantInfo().getTravelDocumentExpirationDate()));
+        a -> AbstractMapper.naIfBlank(a.getApplicantInfo().getTravelDocumentExpirationDate()));
     pdfFieldMap.put(
         "form1[0].#subform[0].PtAILine9_InCareOf[0]",
-        a -> naIfBlank(a.getApplicantInfo().getUsMailingAddress().getInCareOf()));
+        a -> AbstractMapper.naIfBlank(a.getApplicantInfo().getUsMailingAddress().getInCareOf()));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField7[0]", a -> a.getApplicantInfo().getNativeLanguage());
     pdfFieldMap.put(
         "form1[0].#subform[0].CheckBox4[0]",
-        a -> formatCheckbox(a.getApplicantInfo().isFluentInEnglish(), "Yes"));
+        a -> AbstractMapper.formatCheckbox(a.getApplicantInfo().isFluentInEnglish(), "Yes"));
     pdfFieldMap.put(
         "form1[0].#subform[0].CheckBox4[1]",
-        a -> formatCheckbox(!a.getApplicantInfo().isFluentInEnglish(), "No"));
+        a -> AbstractMapper.formatCheckbox(!a.getApplicantInfo().isFluentInEnglish(), "No"));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField7[1]",
-        a -> noneIfBlank(String.join(",", a.getApplicantInfo().getOtherLanguages())));
+        a ->
+            AbstractMapper.noneIfBlank(String.join(",", a.getApplicantInfo().getOtherLanguages())));
     pdfFieldMap.put(
         "form1[0].#subform[0].TextField1[8]",
-        a -> noneIfBlank(a.getApplicantInfo().getUSISAccountNumber()));
+        a -> AbstractMapper.noneIfBlank(a.getApplicantInfo().getUSISAccountNumber()));
 
     // Using map.entries causes a compile warning so im reverting to this
     I589ApplicationMapping.PDF_FIELD_MAP = Collections.unmodifiableMap(pdfFieldMap);
-  }
-
-  private static String noneIfBlank(String input) {
-    return replaceIfBlank(input, "NONE");
-  }
-
-  private static String naIfBlank(String input) {
-    return replaceIfBlank(input, "N/A");
-  }
-
-  private static String replaceIfBlank(String input, String replacement) {
-    return StringUtils.isBlank(input) ? replacement : input;
-  }
-
-  /*
-   * Checkboxes all define their own value for "on" but have the same off value
-   * if the condition is true this will return the checkbox on value. Otherwise it will
-   * return "Off"
-   */
-  private static String formatCheckbox(boolean condition, String checkedValue) {
-    return condition ? checkedValue : "Off";
-  }
-
-  private static <T> Optional<T> safeListAccess(List<T> list, int index) {
-    if (index < 0 || index >= list.size()) {
-      return Optional.empty();
-    } else {
-      return Optional.of(list.get(index));
-    }
   }
 }
