0a0f8e7feb80a6bc27f07562dbf00940b59e91ce
RIchard Rast <richard.rast@gmail.com>
2019-01-19 13:11:54 -0500

    Ran spotless

--- encryption/src/main/java/com/diyasylum/encryption/SecretBox.java
+++ encryption/src/main/java/com/diyasylum/encryption/SecretBox.java
@@ -6,57 +6,56 @@ import com.muquit.libsodiumjna.exceptions.SodiumLibraryException;
 import java.util.Optional;
 
 public class SecretBox {
-    private String password; 
+  private String password;
 
-    public SecretBox(String password) 
-    { 
-        LibsodiumLibrary.initialize();
-        this.password = password; 
-    }
+  public SecretBox(String password) {
+    LibsodiumLibrary.initialize();
+    this.password = password;
+  }
 
-    public Optional<EncryptedMessage> encrypt(String message) {
-    	try {
-    		byte[] salt = SodiumLibrary.randomBytes(SodiumLibrary.cryptoPwhashSaltBytes());
-    		byte[] key = SodiumLibrary.cryptoPwhashArgon2i(password.getBytes(), salt);
-    		byte[] nonce = SodiumLibrary.randomBytes(SodiumLibrary.cryptoBoxNonceBytes().intValue());
-    		byte[] encryptedMessage = SodiumLibrary.cryptoSecretBoxEasy(message.getBytes(), nonce, key);
-    		return Optional.of(new EncryptedMessage(encryptedMessage, salt, nonce));
-    	} catch (SodiumLibraryException e){
-    		System.out.println("Sodium could not encrypt message.");
-      		e.printStackTrace();
-      		return Optional.empty();
-    	}
+  public Optional<EncryptedMessage> encrypt(String message) {
+    try {
+      byte[] salt = SodiumLibrary.randomBytes(SodiumLibrary.cryptoPwhashSaltBytes());
+      byte[] key = SodiumLibrary.cryptoPwhashArgon2i(password.getBytes(), salt);
+      byte[] nonce = SodiumLibrary.randomBytes(SodiumLibrary.cryptoBoxNonceBytes().intValue());
+      byte[] encryptedMessage = SodiumLibrary.cryptoSecretBoxEasy(message.getBytes(), nonce, key);
+      return Optional.of(new EncryptedMessage(encryptedMessage, salt, nonce));
+    } catch (SodiumLibraryException e) {
+      System.out.println("Sodium could not encrypt message.");
+      e.printStackTrace();
+      return Optional.empty();
     }
+  }
 
-    public Optional<String> decrypt(EncryptedMessage encryptedMessage) {
-    	try{
-    		byte[] key = SodiumLibrary.cryptoPwhashArgon2i(password.getBytes(), encryptedMessage.getSalt());
-    		byte[] message = SodiumLibrary.cryptoSecretBoxOpenEasy(encryptedMessage.getEncryptedMessage(), encryptedMessage.getNonce(), key);
-    		return Optional.of(new String(message));
-    	} catch (SodiumLibraryException e) {
-    		System.out.println("Sodium could not decrypt message.");
-      		e.printStackTrace();
-      		return Optional.empty();
-    	}
+  public Optional<String> decrypt(EncryptedMessage encryptedMessage) {
+    try {
+      byte[] key =
+          SodiumLibrary.cryptoPwhashArgon2i(password.getBytes(), encryptedMessage.getSalt());
+      byte[] message =
+          SodiumLibrary.cryptoSecretBoxOpenEasy(
+              encryptedMessage.getEncryptedMessage(), encryptedMessage.getNonce(), key);
+      return Optional.of(new String(message));
+    } catch (SodiumLibraryException e) {
+      System.out.println("Sodium could not decrypt message.");
+      e.printStackTrace();
+      return Optional.empty();
     }
+  }
 }
 
-class LibsodiumLibrary 
-{ 
-    private static boolean isInitialized = false; 
-  
-    private static void loadLibrary() 
-    { 
-    	String libraryPath = "/usr/local/lib/libsodium.so";
-		SodiumLibrary.setLibraryPath(libraryPath);
-		String v = SodiumLibrary.libsodiumVersionString();
-    } 
-  
-    public static void initialize() 
-    { 
-        if (!isInitialized) {
-        	loadLibrary(); 
-        	isInitialized = true;
-        }
-    } 
-} 
+class LibsodiumLibrary {
+  private static boolean isInitialized = false;
+
+  private static void loadLibrary() {
+    String libraryPath = "/usr/local/lib/libsodium.so";
+    SodiumLibrary.setLibraryPath(libraryPath);
+    String v = SodiumLibrary.libsodiumVersionString();
+  }
+
+  public static void initialize() {
+    if (!isInitialized) {
+      loadLibrary();
+      isInitialized = true;
+    }
+  }
+}
