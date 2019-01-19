283720895e3905c580e63b57437ee2cb1aee1c63
Mike <michael.c.kreisel@gmail.com>
2019-01-18 22:32:58 -0500

    basic module for symmetric key encryption

--- encryption/src/main/java/com/diyasylum/encryption/SecretBox.java
+++ encryption/src/main/java/com/diyasylum/encryption/SecretBox.java
@@ -3,6 +3,7 @@ package com.diyasylum.encryption;
 import com.diyasylum.encryption.models.EncryptedMessage;
 import com.muquit.libsodiumjna.SodiumLibrary;
 import com.muquit.libsodiumjna.exceptions.SodiumLibraryException;
+import java.util.Optional;
 
 public class SecretBox {
     private String password; 
@@ -11,31 +12,31 @@ public class SecretBox {
     { 
         LibsodiumLibrary.initialize();
         this.password = password; 
-    } 
+    }
 
-    public EncryptedMessage encrypt(String message) {
+    public Optional<EncryptedMessage> encrypt(String message) {
     	try {
     		byte[] salt = SodiumLibrary.randomBytes(SodiumLibrary.cryptoPwhashSaltBytes());
     		byte[] key = SodiumLibrary.cryptoPwhashArgon2i(password.getBytes(), salt);
     		byte[] nonce = SodiumLibrary.randomBytes(SodiumLibrary.cryptoBoxNonceBytes().intValue());
     		byte[] encryptedMessage = SodiumLibrary.cryptoSecretBoxEasy(message.getBytes(), nonce, key);
-    		return new EncryptedMessage(encryptedMessage, salt, nonce);
+    		return Optional.of(new EncryptedMessage(encryptedMessage, salt, nonce));
     	} catch (SodiumLibraryException e){
-    		System.out.println("Sodium failure");
+    		System.out.println("Sodium could not encrypt message.");
       		e.printStackTrace();
-      		return null;
+      		return Optional.empty();
     	}
     }
 
-    public String decrypt(EncryptedMessage encryptedMessage) {
+    public Optional<String> decrypt(EncryptedMessage encryptedMessage) {
     	try{
     		byte[] key = SodiumLibrary.cryptoPwhashArgon2i(password.getBytes(), encryptedMessage.getSalt());
     		byte[] message = SodiumLibrary.cryptoSecretBoxOpenEasy(encryptedMessage.getEncryptedMessage(), encryptedMessage.getNonce(), key);
-    		return new String(message);
+    		return Optional.of(new String(message));
     	} catch (SodiumLibraryException e) {
-    		System.out.println("Sodium failure");
+    		System.out.println("Sodium could not decrypt message.");
       		e.printStackTrace();
-      		return null;
+      		return Optional.empty();
     	}
     }
 }
@@ -47,13 +48,8 @@ class LibsodiumLibrary
     private static void loadLibrary() 
     { 
     	String libraryPath = "/usr/local/lib/libsodium.so";
-    	System.out.println("Loading libsodium");
-    	System.out.println("Library path: " + libraryPath);
 		SodiumLibrary.setLibraryPath(libraryPath);
-		// To check the native library is actually loaded, print the version of 
-		// native sodium library
 		String v = SodiumLibrary.libsodiumVersionString();
-		System.out.println("libsodium version: " + v); 
     } 
   
     public static void initialize() 
