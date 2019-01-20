df0d1e620cf6d901cb3c25ddb10d2c5ebb66cd58
Matt Bachmann <bachmann.matt@gmail.com>
2019-01-19 22:45:18 -0500

    Setup build cache for libsodium

--- encryption/scripts/installLibSodium.sh
+++ encryption/scripts/installLibSodium.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-curl https://download.libsodium.org/libsodium/releases/libsodium-1.0.17.tar.gz > libsodium-1.0.17.tar.gz
-tar -xvf libsodium-1.0.17.tar.gz
+if [[ ! -d libsodium-1.0.17 ]]; then
+    curl https://download.libsodium.org/libsodium/releases/libsodium-1.0.17.tar.gz > libsodium-1.0.17.tar.gz
+    tar -xvf libsodium-1.0.17.tar.gz
+fi
 cd libsodium-1.0.17
 ./configure
 make && make check
